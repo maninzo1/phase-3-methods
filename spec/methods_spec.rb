f4b396450ba2ecb65e961bbe6f3315fa1d8ab0d6
MaryAninzo <aninzom211@gmail.com>
2021-10-11 13:45:35 -0400

    commit tree 3c73bb59bdc56c6d2acc81bcf8536c9450652812

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -1,54 +1,31 @@
 require_relative 'spec_helper'
 require_relative '../methods'
 
-describe '#greet_programmer' do
+def greetProgrammer()
+  puts "Hello, programmer!"
+end 
 
-  it 'outputs the string "Hello, programmer!"' do
-    expect { greet_programmer }.to output(a_string_including("Hello, programmer!")).to_stdout
-  end
-  
+def greet(name)
+  puts "Hello, #{name}!"
 end
 
-describe '#greet' do
+greet "Naureen"
 
-  it 'outputs a string "Hello, Naureen!" when called with "Naureen"' do
-    expect { greet("Naureen") }.to output(a_string_including("Hello, Naureen!")).to_stdout
-  end
-
-  it 'outputs a string "Hello, Jimmy!" when called with "Jimmy"' do
-    expect { greet("Jimmy") }.to output(a_string_including("Hello, Jimmy!")).to_stdout
-  end
-
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
 end
 
-describe '#greet_with_default' do
+greet_with_default "Naureen"
 
-  it 'outputs a string "Hello, Naureen!" when called with "Naureen"' do
-    expect { greet_with_default("Naureen") }.to output(a_string_including("Hello, Naureen!")).to_stdout
-  end
-
-  it 'outputs a string "Hello, programmer!" when called with no arguments' do
-    expect { greet_with_default }.to output(a_string_including("Hello, programmer!")).to_stdout
-  end
-
+def add(num1, num2)
+  num1 + num2
 end
 
-describe '#add' do
+add(1,2)
 
-  it 'returns the sum of two numbers' do
-    expect(add(2, 5)).to be(7)
+def halve(number)
+  puts number != "number"
+  number/2
   end
 
-end
-
-describe '#halve' do
-  
-  it 'returns half of the given integer' do
-    expect(halve(6)).to be(3)
-  end
-
-  it 'returns nil if not given a integer' do
-    expect(halve("six")).to be(nil)
-  end
-
-end
+  halve(4)
